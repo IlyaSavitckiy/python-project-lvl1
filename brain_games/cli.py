29e925ba99fe4cf32b1c3b7f916304f8f7bb155d
Ilya Savitskiy <hex.ilyasavitskiy@gmail.com>
2020-06-14 08:11:50 +0300

    add flake8, TravisCI and CodeClimate

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,5 +1,9 @@
+"""File with foo to greet operator."""
+
 import prompt
 
+
 def welcome_user():
-	name = prompt.string('May I have your name? ')
-	print(f'Hello, {name}!')
+    """Foo that greet people."""
+    name = prompt.string('May I have your name? ')
+    print('Hello, {a}!'.format(a=name))
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,12 +1,13 @@
+"""Module."""
+
 from brain_games import cli
 
 
 def main():
+    """Foo with greeting."""
     print('Welcome to the Brain Games!')
     cli.welcome_user()
 
+
 if __name__ == '__main__':
     main()
-    
-
-    
